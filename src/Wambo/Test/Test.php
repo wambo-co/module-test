204233093b586cd5bf7efcbd1771e58d8c5ba94e
Rene Penner <rene@penner.name>
2016-07-08 20:26:21 +0200

    Set composer file to extra -> class for bootstraping a wambo module

---
+++ src/Wambo/Test/Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Wambo\Test;
+
+use Slim\Http\Response;
+use Wambo\Core\App;
+use Wambo\Core\Module\ModuleBootstrapInterface;
+
+class Test implements ModuleBootstrapInterface
+{
+    /**
+     * @param $app \Wambo\Core\App
+     */
+    public function __construct(App $app)
+    {
+        // Get container
+        $container = $app->getContainer();
+
+        $app->get('/', function ($request, $response, $args) {
+            /** @var $response Response */
+            $response->write("Hello form Test Module!");
+            return $response;
+        });
+    }
+}
