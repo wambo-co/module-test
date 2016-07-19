380c93d97d341fd8115b880d405900e738b28c54
Rene Penner <rene@penner.name>
2016-07-19 23:22:24 +0200

    add dummy php unit tests

---
+++ tests/Wambo/Test/Dummy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Wambo\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class Dummy
+ *
+ * @package Wambo\Test
+ */
+class Dummy extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
